b33658ef96284db5da8fc69d0e54ec61f8f172e0
TrexChoong <guanstrife@gmail.com>
2023-05-08 08:32:57 +0800

    commit new page

--- src/js/app.js
+++ src/js/app.js
@@ -71,10 +71,10 @@ App = {
     $.getJSON('SponsorAnimals.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
       var AdoptionArtifact = data;
-      //App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
     
       // Set the provider for our contract
-      //App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.Adoption.setProvider(App.web3Provider);
     
       // Use our contract to retrieve and mark the adopted animals
       //return App.markAdopted();
